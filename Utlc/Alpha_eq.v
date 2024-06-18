d186e77351cd6dce33b0d95c51f7ac901045ee9d
ratpi fonctionnel <wontusemailcom@protonmail.com>
2024-06-18 23:21:22 +0200

    erasing old false free variable counting

--- Utlc/Alpha_eq.v
+++ Utlc/Alpha_eq.v
@@ -1,4 +1,5 @@
 Require Import Nat.
+Require Import List.
 Require Import UTLC.LC.
 
 (*
@@ -17,28 +18,17 @@ Proof.
   - exact (1 + nb_binders t').
 Qed.
 
-Fixpoint count_fv_from (t : LC) (n : nat) : nat.
+Fixpoint count_fv_from (t : LC) (fv_list : list nat) : nat * list nat.
 Proof.
   destruct t as [var | [t1 t2] | t'].
-  - exact (if var <=? n then 0 else 1).
-  - exact (count_fv_from t2 (count_fv_from t1 n)). 
-  - exact (count_fv_from t' (n+1)).
+  - remember (existsb (fun x => x =? var) fv_list) as b.
+    exact (if b then 0 else 1, if b then fv_list else var :: fv_list). 
+  - remember (count_fv_from t1 fv_list) as left_f.
+    remember (count_fv_from t2 (snd left_f)) as right_f.
+    exact (fst left_f + fst right_f ,  snd right_f). 
+  - remember (map  (fun x => x+1) fv_list) as lifted_fv_list.
+    exact ( fst (count_fv_from t' lifted_fv_list) , fv_list).
 Qed.
 
-Definition nb_fv (t : LC) : nat := count_fv_from t 0.
-
-Fixpoint min_var (t : LC) : nat.
-Proof.
-  destruct t as [var | [t1 t2] | t'].
-  - exact var.
-  - exact (min (min_var t1) (min_var t2)).
-  - exact (min_var t').
-Qed.  
-
-Fixpoint max_var (t : LC) : nat.
-Proof.
-  destruct t as [var | [t1 t2] | t'].
-  - exact var.
-  - exact (max (max_var t1) (max_var t2)).
-  - exact (max_var t').
-Qed.
+Definition count_fv (t : LC) : nat := fst (count_fv_from t nil).
+                                    
