865f3a39ac956bdcae70ae39552c46b6f3164550
ratpi fonctionnel <wontusemailcom@protonmail.com>
2024-06-20 13:26:32 +0200

    splitting examples + well formed terms

--- Utlc/Alpha_eq.v
+++ Utlc/Alpha_eq.v
@@ -2,22 +2,6 @@ Require Import Nat.
 Require Import List.
 Require Import UTLC.LC.
 
-(*
-α-equivalence is syntactic equality for the representation with De Bruijin indexes, under the following naming convention when building terms :
-
-1) We start free variable indexes right after the number of binders.
-2) We start counting variables from 1.  
-
-*)
-
-Fixpoint nb_binders (t : LC) : nat.
-Proof.
-  destruct t as [var | [t1 t2] | t'].
-  - exact 0.
-  - exact (nb_binders t1).
-  - exact (1 + nb_binders t').
-Qed.
-
 Fixpoint count_fv_from (t : LC) (fv_list : list nat) : nat * list nat.
 Proof.
   destruct t as [var | [t1 t2] | t'].
@@ -26,9 +10,25 @@ Proof.
   - remember (count_fv_from t1 fv_list) as left_f.
     remember (count_fv_from t2 (snd left_f)) as right_f.
     exact (fst left_f + fst right_f ,  snd right_f). 
-  - remember (map  (fun x => x+1) fv_list) as lifted_fv_list.
+  - remember (map S fv_list) as lifted_fv_list.
     exact ( fst (count_fv_from t' lifted_fv_list) , fv_list).
 Qed.
 
-Definition count_fv (t : LC) : nat := fst (count_fv_from t nil).
-                                    
+Definition nb_fv (t : LC) : nat := fst (count_fv_from t nil).
+
+Fixpoint well_formed_from (t : LC) (n : nat) : Prop .
+Proof.
+  destruct t as [var | [t1 t2] | t'].
+  - exact (var <= n /\ 1 <= var).
+  - exact ((well_formed_from t1 n) /\ (well_formed_from t2 (nb_fv t1))). 
+  - exact (well_formed_from t' (S n)).
+Qed.
+
+Definition well_formed : LC -> Prop.
+Proof.
+  intro t.
+  exact (well_formed_from t 1).
+Qed.
+  
+Definition WLC : Set := {t : LC | well_formed t}.
+ 
---
+++ Utlc/Examples.v
@@ -0,0 +1,20 @@
+Require Import Nat.
+Require Import List.
+Require Import UTLC.LC.
+Require Import UTLC.Alpha_eq.
+
+
+Definition Id : LC :=  Abs (Var 1).
+
+Definition K : LC := Abs (Abs (Var 2)).
+
+Definition K' : LC := Abs (Abs (Var 1)).
+
+Definition S : LC := Abs (Abs (Abs (App ((App (Var 3 , Var 1)), (App (Var 2 , Var 1)))))).
+
+Definition ω : LC := Abs (App (Var 1, Var 1)).
+
+Definition Ω : LC := App (ω, ω). 
+
+Definition Y : LC := Abs (App (Abs (App ( Var 2, App (Var 1, Var 1))) , Abs (App ( Var 2, App (Var 1, Var 1))))).
+
